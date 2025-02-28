b6792b775f3be3ac2511ab9281466d25f620f8dc
小金平 <290762142@qq.com>
2025-02-28 18:18:48 +0800

    vip 页面采用 模板的方式导入

--- gulpfile.js
+++ gulpfile.js
@@ -4,12 +4,19 @@ const prettify = require('gulp-prettify');
 const path = require('path');
 const fs = require('fs'); // 使用 Node.js 的 fs 模块
 
+const paths = {
+  templates: path.resolve(__dirname, 'templates'),  // 模板文件所在路径
+  src: path.resolve(__dirname, 'src')               // HTML 文件所在路径
+};
 // 处理模板并输出到 pages 目录，且格式化 HTML
 gulp.task('html', () => {
+  const basePath = path.resolve(__dirname, ''); // 'src' 为存放模板文件的目录，您可以根据需要修改
+  console.log("absePath", basePath)
   return gulp.src('src/**/*.html') // 匹配 src 目录下的所有 .html 文件
     .pipe(fileInclude({
       prefix: '@@',             // 模板标记
-      basepath: '@file',        // 基础路径设置为当前文件
+      basepath: basePath,        // 基础路径设置为当前文件
+    
     })) // 引入模板的标记，如 @@include('_header.html')
     .pipe(prettify({
       indent_size: 4,           // 缩进大小，单位为空格
@@ -22,8 +29,15 @@ gulp.task('html', () => {
     .pipe(gulp.dest('pages'));
 });
 
+
 // 实时监控模板文件和其他源文件的变化
 gulp.task('watch', () => {
+  gulp.watch('js/**/*.js', gulp.series('html')); // 监控 src 目录下所有的 .html 文件
+  // gulp.watch('css/**/*.css', gulp.series('html')); // 监控 src 目录下所有的 .html 文件
+  gulp.watch('src/**/*.js', gulp.series('html')); // 监控 src 目录下所有的 .html 文件
+  gulp.watch('src/**/*.ejs', gulp.series('html')); // 监控 src 目录下所有的 .html 文件
+  gulp.watch('src/**/*.css', gulp.series('html')); // 监控 src 目录下所有的 .html 文件
+  gulp.watch('src/**/*.tpl', gulp.series('html')); // 监控 src 目录下所有的 .html 文件
   gulp.watch('src/**/*.html', gulp.series('html')); // 监控 src 目录下所有的 .html 文件
 });
 
