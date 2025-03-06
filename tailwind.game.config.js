e2aaa51b63ef6d10629ebf0dbf5bdb1294e6a0ef
小金平 <290762142@qq.com>
2025-03-06 18:52:59 +0800

    3

---
+++ tailwind.game.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  content: [
+    "./pages/game-booster/*.amp.html",
+    "./src/game-booster/tpl/*.ejs",
+    "./src/game-booster/*.amp.html",
+    "./templates/htmlTpl/*.ejs",
+    "./templates/ampTpl/*.ejs"
+  ],
+
+	safelist: [],
+  theme: {
+    extend: {
+      colors: {
+      },
+    },
+  },
+  plugins: [],
+}
