b91a164d205243f384b68fecdfc4ee864f835280
小金平 <290762142@qq.com>
2025-03-06 10:16:24 +0800

    每个文件都生成一个对应的tw.amp.css

---
+++ tailwind.home.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  content: [
+    "./pages/home/*.html",
+    "./src/home/**/*.ejs",
+    "./src/home/**.html",
+    "./templates/htmlTpl/*.ejs",
+    "./templates/ampTpl/*.ejs"
+  ],
+
+	safelist: [],
+  theme: {
+    extend: {
+      colors: {
+      },
+    },
+  },
+  plugins: [],
+}
---
+++ tailwind.media.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  content: [
+    "./pages/media-accel-list/*.amp.html",
+    "./src/media-accel-list/tpl/*.ejs",
+    "./src/media-accel-list/*.amp.html",
+    "./templates/htmlTpl/*.ejs",
+    "./templates/ampTpl/*.ejs"
+  ],
+
+	safelist: [],
+  theme: {
+    extend: {
+      colors: {
+      },
+    },
+  },
+  plugins: [],
+}
