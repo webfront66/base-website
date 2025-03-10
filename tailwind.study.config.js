590e3b3ecd4b3b87e99e5be6a3b53db10edd134a
小金平 <290762142@qq.com>
2025-03-10 09:55:42 +0800

    2

--- tailwind.study.config.js
+++ tailwind.study.config.js
@@ -1,8 +1,23 @@
 module.exports = {
   content: [
-    "./pages/study/study.amp.html",
+    "./pages/study/*.amp.html",
+    "./pages/overseas/*.amp.html",
+    "./pages/travel/*.amp.html",
+    "./pages/office/*.amp.html",
+    "./pages/live/*.amp.html",
+
     "./src/study/tpl/*.ejs",
-    "./src/study/study.amp.html",
+    "./src/overseas/tpl/*.ejs",
+    "./src/travel/tpl/*.ejs",
+    "./src/office/tpl/*.ejs",
+    "./src/live/tpl/*.ejs",
+
+    "./src/study/*.amp.html",
+    "./src/overseas/*.amp.html",
+    "./src/travel/*.amp.html",
+    "./src/office/*.amp.html",
+    "./src/live/*.amp.html",
+
     "./templates/htmlTpl/*.ejs",
     "./templates/ampTpl/*.ejs"
   ],
