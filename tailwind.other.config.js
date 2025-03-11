7c5febd322a66ebe58f0bd84b75aeb6426bf1b6d
小金平 <290762142@qq.com>
2025-03-11 19:21:12 +0800

    re

---
+++ tailwind.other.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  content: [
+    "./pages/other/*.html",
+    "./src/other/**/*.{html,ejs}",
+    "./src/other/**.html",
+    "./templates/htmlTpl/*.ejs",
+    "./templates/ampTpl/*.ejs"
+  ],
+
+	safelist: [],
+  theme: {
+    extend: {
+      colors: {
+      },
+    },
+  },
+  plugins: [],
+}
