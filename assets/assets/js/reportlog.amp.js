8008bcf2ea0b91e7ea191bc790b8f1cca95f51fb
frontweb66 <frontweb66@gmail.com>
2025-08-10 15:18:23 +0800

    35

--- assets/assets/js/reportlog.amp.js
+++
@@ -1,9 +0,0 @@
-AMP.getState('reportLogInfo').then((res) => {
-	fetch('https://admin.fqhgvpn.com/analyze/reportLog', {
-		method: 'POST',
-		headers: {
-			'Content-Type': 'application/json',
-		},
-		body: res,
-	})
-});
