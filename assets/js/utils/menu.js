aef6b41290b7a63397df47e6722b8591cd4281ce
frontweb66 <frontweb66@gmail.com>
2025-08-17 01:02:36 +0800

    fea:去掉日志

--- assets/js/utils/menu.js
+++ assets/js/utils/menu.js
@@ -34,18 +34,5 @@ document.addEventListener('DOMContentLoaded', function () {
 			}
 			return false;
 	}
-	function reportLog () {
-		const res = {
-			"host": window.location.host,
-			"path": window.location.pathname
-		}
-		fetch('https://admin.fqhgvpn.com/analyze/reportLog', {
-			method: 'POST',
-			headers: {
-				'Content-Type': 'application/json',
-			},
-			body: JSON.stringify(res),
-		})
-	}
-	reportLog()
+	
 });
